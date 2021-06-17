eed8ab4ca21bc0286d162cf7297d752800a69998
Ewan Lyon <clubwhomc@gmail.com>
2021-06-17 12:01:24 +1000

    Add couch and asmbanner to widescreen

--- src/graphics/overlays/widescreen.tsx
+++ src/graphics/overlays/widescreen.tsx
@@ -8,6 +8,8 @@ import { Timer } from '../elements/timer';
 import { SponsorsBox } from '../elements/sponsors';
 import * as RunInfo from '../elements/run-info';
 import { OrangeStripe } from '../elements/orange-stripe';
+import { Couch } from '../elements/couch';
+import { ASMBanner } from '../elements/asm-banner';
 
 const WidescreenContainer = styled.div`
 	height: 1016px;
@@ -19,17 +21,26 @@ const Sidebar = styled.div`
 	top: 156px;
 	height: 860px;
 	width: 390px;
-	border-right: 1px solid var(--main-col);
+	border-right: 1px solid var(--asm-orange);
+	/* border-top: 1px solid var(--asm-orange); */
+`;
+
+const SidebarBG = styled.div`
+	background: var(--main-col);
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 446px;
+	padding-top: 14px;
 `;
 
 const SponsorBoxS = styled(SponsorsBox)`
-	position: absolute;
+	/* position: absolute; */
 	width: 100%;
-	height: 459px;
-	left: 0px;
-	top: 400px;
-	background: var(--main-col);
-	border-top: 1px solid var(--main-col);
+	/* height: 459px; */
+	flex-grow: 1;
+	/* left: 0px;
+	top: 400px; */
 	overflow: hidden;
 	display: flex;
 	justify-content: center;
@@ -38,6 +49,7 @@ const SponsorBoxS = styled(SponsorsBox)`
 
 const InfoBar = styled.div`
 	background: var(--main-col);
+	border-bottom: 1px var(--asm-orange) solid;
 	position: absolute;
 	height: 156px;
 	width: 1920px;
@@ -89,20 +101,27 @@ export const Widescreen: React.FC<OverlayProps> = (props) => {
 						category={props.runData?.category || ''}
 						style={{ marginBottom: -25 }}
 					/>
-					<RunInfo.Estimate estimate={props.runData?.estimate || ''} />
+					<RunInfo.Estimate
+						estimate={props.runData?.estimate || ''}
+					/>
 				</VerticalStack>
 				<InfoDivider />
 				<Timer style={{ width: 612, zIndex: 2 }} timer={props.timer} />
 			</InfoBar>
 			<Sidebar>
-				<Facecam
-					height={400}
-					teams={props.runData?.teams[0]}
-				/>
+				<Facecam height={400} teams={props.runData?.teams[0]} />
+				<SidebarBG>
+					<Couch
+						couch={props.preview ? props.couchInformation.preview : props.couchInformation.current}
+					/>
+					<ASMBanner style={{marginTop: 4}} />
+					<SponsorBoxS
+						sponsorStyle={SponsorSize}
+						tweetStyle={TwitterSize}
+					/>
 
-				<SponsorBoxS sponsorStyle={SponsorSize} tweetStyle={TwitterSize} />
-				
-				<OrangeStripe side='bottom' style={{position: 'absolute', bottom: 0, width: '100%'}} />
+					<OrangeStripe side="bottom" style={{ width: '100%' }} />
+				</SidebarBG>
 			</Sidebar>
 		</WidescreenContainer>
 	);
