553b738fdfb9c1278e5a755bd6571b80da89a2ef
Ewan Lyon <clubwhomc@gmail.com>
2021-07-11 22:51:19 +1000

    Reverted back to normal streams

--- src/graphics/stream.tsx
+++ src/graphics/stream.tsx
@@ -7,7 +7,7 @@ import { ASMStream } from './elements/individual-stream';
 
 const StreamContainer = styled.div``;
 
-const Streams = ['liquidwifi', 'esl_csgo', 'xisuma', 'capcomfighters'];
+const Streams = ['asm_station1', 'asm_station2', 'asm_station3', 'asm_station4'];
 
 export const Stream: React.FC = () => {
 	const StreamElements = Streams.map((stream) => {
