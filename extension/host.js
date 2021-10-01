2389a9dc859d7bc884fecbcdc0caecfac934bce2
Ewan Lyon <clubwhomc@gmail.com>
2021-10-01 11:29:36 +1000

    Removed extension build folder

--- extension/host.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const nodecgApiContext = tslib_1.__importStar(require("./nodecg-api-context"));
-const nodecg = nodecgApiContext.get();
-const hostNameRep = nodecg.Replicant('host');
-nodecg.listenFor('update-hostname', (data) => {
-    hostNameRep.value = data;
-});
